182f81fd220ddf7a9f13b274ca28a93e9a7d096f
aaaron27 <a.perez.1@estudiantec.cr>
2026-03-04 11:09:28 -0600

    add template

--- main.c
+++ main.c
@@ -1,5 +1,35 @@
 #include <stdio.h>
+#include <pthread.h>
+
+typedef struct {
+    unsigned int speed;
+    int isAmbulance; // true-false
+    int type;
+} Car;
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void* foo(void* arg) {
+    // region critica
+    pthread_mutex_lock(&mutex);
+
+    pthread_mutex_unlock(&mutex);
+
+    return NULL;
+}
 
 int main() {
+    // inicializacion de hilos
+    pthread_t t1;
+
+    // creacion del hilo
+    pthread_create(&t1, NULL, foo, NULL);
+
+    // sincronizacion del hilo con el main
+    pthread_join(t1, NULL);
+
+    // destruir el mutex
+    pthread_mutex_destroy(&mutex);
+    
     return 0;
 }
